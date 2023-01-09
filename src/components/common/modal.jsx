3228d80498ad12f480f397fbe935b47687284cbb
aaron <tzuyuan.yu@leishan.com.tw>
2023-01-09 11:32:19 +0800

    [feat] add ssg config

--- src/components/common/modal.jsx
+++ src/components/common/modal.jsx
@@ -1,6 +1,6 @@
 import { component$, useStylesScoped$, Slot } from '@builder.io/qwik';
 import style from './modal.scss?inline';
-import Button from './button';
+import Button from '@/components/bmsSample/button';
 
 export default component$(({ title = '標題', closeHandler$ }) => {
   useStylesScoped$(style);
@@ -14,7 +14,11 @@ export default component$(({ title = '標題', closeHandler$ }) => {
           <div class="title">{title}</div>
           <Slot name="body" />
           <div class="bottom">
-            <Button type="cancel" text="關閉" clickHandler$={closeHandler$} />
+            <Button
+              buttonWord="關閉"
+              buttonStyle="blue"
+              onClick$={closeHandler$}
+            />
           </div>
         </div>
       </div>
